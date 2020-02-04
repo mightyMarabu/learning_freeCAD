47011925ff68419f59638029f18c88985e7ecfc1
mightymarabu <sebastian.n.schmidt@web.de>
2020-02-04 17:26:15 +0100

    inital commit

---
+++ first_try.py
@@ -0,0 +1,15 @@
+#### doku: https://www.freecadweb.org/wiki/Python_scripting_tutorial ####
+
+
+# create a new document
+testdoc = FreeCAD.newDocument()
+# create a box
+box = testdoc.addObject("Part::Box","myBox")
+# recompute document
+testdoc.recompute()
+
+##check doc functions
+testdoc.
+
+#check box functions
+box.
