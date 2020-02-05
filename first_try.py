e28c174c28b29c7008d39425fede4cf149ed83f6
Sebastian <35834351+mightyMarabu@users.noreply.github.com>
2020-02-05 08:44:40 +0100

    Update first_try.py

--- first_try.py
+++ first_try.py
@@ -13,3 +13,18 @@ testdoc.
 
 #check box functions
 box.
+
+
+#######
+
+def createBox(a,b,c):
+   a = 3
+   b = 3   
+   c = 3
+   boxdoc = FreeCAD.newDocument()
+   box = boxdoc.addObject("Part::Box","myBox")
+   boxdoc.recompute()
+   box.Height = a
+   box.Length = b
+   box.Width = c
+   boxdoc.recompute()
